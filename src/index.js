420a112648b38b7ae85345e248889fe319468ab9
Manav Majithiya <91357474+manav282@users.noreply.github.com>
2023-08-01 21:30:53 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from './redux/store';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-        <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/checklist" element={<App />}/>
+      </Routes>
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
